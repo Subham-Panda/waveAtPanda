8ada15faf85b6776d0bba4dc0b8cebf894c623d7
Subham Subhasish Panda <im.sspanda10@gmail.com>
2022-11-12 15:36:04 +0530

    feat: first dummy samart contract

---
+++ contracts/WaveAtPanda.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract WaveAtPanda {
+    constructor() {
+        console.log("Yippee, worte my first smart contract");
+    }
+}
