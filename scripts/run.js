0707ae8755782b07e1f3d1aab1516179dbea538f
Subham Subhasish Panda <im.sspanda10@gmail.com>
2022-11-12 16:36:44 +0530

    feat: let people wave and store waves

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,18 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WaveAtPanda");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
-    console.log("WaveAtPanda contract is deployed at: ",waveContract.address)
+
+    console.log("WaveAtPanda contract is deployed at: ",waveContract.address);
+    console.log("WaveAtPanda contract deployed by: ", owner.address);
+
+    await waveContract.getTotalWaves();
+
+    const waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    await waveContract.getTotalWaves();
 }
 
 const runMain = async () => {
